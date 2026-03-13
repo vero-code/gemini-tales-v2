6414fa02997b40e5f4a5ca0c428fb1b59f8027fd
vero-code <79207983+vero-code@users.noreply.github.com>
2026-03-13 16:26:57 +0200

    feat: add adk-streaming agent

---
+++ backend/adk-streaming/app/google_search_agent/agent.py
@@ -0,0 +1,17 @@
+from google.adk.agents import Agent
+from google.adk.tools import google_search  # Import the tool
+
+root_agent = Agent(
+   # A unique name for the agent.
+   name="basic_search_agent",
+   # The Large Language Model (LLM) that agent will use.
+   # Please fill in the latest model id that supports live from
+   # https://google.github.io/adk-docs/get-started/streaming/quickstart-streaming/#supported-models
+   model="gemini-live-2.5-flash-native-audio",
+   # A short description of the agent's purpose.
+   description="Agent to answer questions using Google Search.",
+   # Instructions to set the agent's behavior.
+   instruction="You are an expert researcher. You always stick to the facts.",
+   # Add google_search tool to perform grounding with Google search.
+   tools=[google_search]
+)
